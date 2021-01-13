729ef3be6a750219117644125440464be671c4e9
Arnau Segura <arnau148@gmail.com>
2021-01-13 21:47:06 +0100

    Experimenting with shinyjqui and shinyWidgets for plot resizing.

--- Network-app/ui.R
+++ Network-app/ui.R
@@ -11,41 +11,53 @@ library(shiny)
 library(shinydashboard)
 library(visNetwork)
 library(igraph)
+library(shinyWidgets)
+library(shinyjqui)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+    useShinydashboard(),
     # Application title
     titlePanel("Random Network"),
     
     # Sidebar with a slider input for number of nodes and edges
-    sidebarLayout(sidebarPanel(
-        sliderInput(
-            "nodes",
-            "Number of Nodes:",
-            min = 1,
-            max = 50,
-            value = 30
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput(
+                "nodes",
+                "Number of Nodes:",
+                min = 1,
+                max = 50,
+                value = 30
+            ),
+            sliderInput(
+                "edges",
+                "Number of Edges:",
+                min = 1,
+                max = 100,
+                value = 30
+            )
         ),
-        sliderInput(
-            "edges",
-            "Number of Edges:",
-            min = 1,
-            max = 100,
-            value = 30
-        )
-    ),
-    
-    # Show a plot of the generated network
-    mainPanel(
-        box(
-            visNetworkOutput("graphPlot"),
-            solidHeader = T,
-            status = "primary",
-            width = "100%",
-            height = 250,
-            title = "Test box",
-            collapsible = T,
-            collapsed = F
-        )
-    ))
+        
+        # Show a plot of the generated network
+        mainPanel(jqui_resizable(
+            box(
+                visNetworkOutput("graphPlot"),
+                solidHeader = T,
+                status = "primary",
+                width = "100%",
+                height = "100%",
+                title = "Test box",
+                collapsible = T,
+                collapsed = F
+            )
+        ))
+    )
 ))
+
+
+
+# Limit the resizable element to a maximum or minimum height or width
+#jqui_resizable('#foo', options = list(minHeight = 100, maxHeight = 300,
+ #                                     minWidth = 200, maxWidth = 400))
+
